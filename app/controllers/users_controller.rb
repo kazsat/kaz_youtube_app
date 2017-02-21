6334ac6aae3c6f3ae2b5d2d1425c0ab5e4f7f4fa
Kazuzki <regaltequieroo@gmail.com>
2017-02-21 14:25:56 +0000

    add strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,24 @@
 class UsersController < ApplicationController
+  #ユーザー登録フォームの表示
   def new
     @user = User.new
   end
   
+  #ユーザー登録
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      #成功時
+    else
+      #失敗時
+      render "new"
+    end
   end
+  
+  
+  #ストロングパラメータ
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+  
 end
