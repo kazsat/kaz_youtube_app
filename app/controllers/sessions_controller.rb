e4abc43ad61474cbcbc8ace6c82bc7d2c42f2842
Kazuzki <regaltequieroo@gmail.com>
2017-02-22 08:59:58 +0000

    basic login and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  #ログインページの表示
+  def new
+  end
+  
+  #ログイン
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #ログイン成功時
+      log_in(user)
+      redirect_to user
+      
+    else
+      #ログイン失敗時
+      flash.now[:danger] = "ログイン情報が正しくありません"
+      render "new"
+    end
+  end
+  
+  #ログアウト
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       #成功時
+      log_in @user
       flash[:success] = "登録が完了しました！"
       redirect_to @user
     else
