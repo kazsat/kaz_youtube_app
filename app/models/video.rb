d688d0dcdde3bd6aabf17317d658c42088ac4bcd
Kazuzki <regaltequieroo@gmail.com>
2017-02-23 13:46:17 +0000

    add regex to url

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,6 +4,8 @@ class Video < ApplicationRecord
   validates :user_id,presence:true
   validates :title,presence:true,length: {maximum:50}
   validates :description ,length: {maximum:300}
-  validates :url,presence:true
+  #urlの正規表現 正しいかチェックの必要あり
+  VALID_URL_REGEX = /(\Ahttps:\/\/www.youtube.com\/watch\?v=)([-\w]{11})\z/
+  validates :url,presence:true,format: {with: VALID_URL_REGEX}
   
 end
